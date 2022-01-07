9dd3af6a0e67db44658759564eb502b46870dbcc
Lemuel Coelho Zara <lemuel.czara@mercadolivre.com>
2022-01-07 18:40:47 -0300

    test: should be throw if district not found

--- src/test/java/com/meli/myproperty/unit/district/FindDistrictByIdUseCaseTest.java
+++ src/test/java/com/meli/myproperty/unit/district/FindDistrictByIdUseCaseTest.java
@@ -2,15 +2,14 @@ package com.meli.myproperty.unit.district;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.fail;
 
 import java.math.BigDecimal;
-import java.security.NoSuchAlgorithmException;
 import java.util.NoSuchElementException;
 
 import com.meli.myproperty.modules.district.domain.District;
 import com.meli.myproperty.modules.district.infra.repository.DistrictRepository;
 import com.meli.myproperty.modules.district.usecases.FindDistrictById.FindDistrictByIdUseCase;
+import com.meli.myproperty.shared.exception.NotFoundElementException;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -39,11 +38,11 @@ public class FindDistrictByIdUseCaseTest {
     public void shouldBeThrowIfDistrictNotFound() {
         Mockito.when(districtRepository.findById("validId")).thenReturn(null);
 
-        NoSuchElementException exception = assertThrows(NoSuchElementException.class, () -> {
+        NotFoundElementException exception = assertThrows(NotFoundElementException.class, () -> {
             useCase.execute("validId");
         });
 
-        assertEquals(exception.getClass(), new NoSuchElementException().getClass());
+        assertEquals(exception.getClass(), NotFoundElementException.class);
         assertEquals(exception.getMessage(), "District not found!");
     }
 
