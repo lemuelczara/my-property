2e600e7854396f9ff962e8448af375b13fc286e1
Lemuel Coelho Zara <lemuel.czara@mercadolivre.com>
2022-01-12 14:52:43 -0300

    test: should be call repository with correct values

--- src/test/java/com/meli/myproperty/unit/district/FindDistrictByIdUseCaseTest.java
+++ src/test/java/com/meli/myproperty/unit/district/FindDistrictByIdUseCaseTest.java
@@ -3,10 +3,11 @@ package com.meli.myproperty.unit.district;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.doCallRealMethod;
 import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.spy;
 
 import com.meli.myproperty.modules.district.infra.repository.DistrictRepository;
@@ -16,6 +17,7 @@ import com.meli.myproperty.unit.district.mocks.DistrictRepositorySpy;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 
 public class FindDistrictByIdUseCaseTest {
     private DistrictRepository districtRepository;
@@ -41,6 +43,17 @@ public class FindDistrictByIdUseCaseTest {
         NotFoundElementException exception = assertThrows(NotFoundElementException.class, () -> sut.execute("validId"));
 
         assertTrue(exception.getMessage().contains("District not found!"));
+    }
+
+    @Test()
+    public void shouldBeCallRepositoryWithCorrectValues() {
+        ArgumentCaptor<String> arg0 = ArgumentCaptor.forClass(String.class);
+
+        doCallRealMethod().when(districtRepository).findById(arg0.capture());
+
+        sut.execute("validId");
+
+        assertEquals(arg0.getValue(), "validId");
     }
 
     private DistrictRepository createDistrictRepository() {
